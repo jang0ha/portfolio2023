7ab2c35e80b926abfb9c1a2e54f8bb69fa092ae0
jang0ha <jang0ha@gmail.com>
2023-03-26 17:01:42 +0900

    fix : 메인 > 어바웃섹션 옵저버

--- src/assets/js/ui.js
+++ src/assets/js/ui.js
@@ -4,6 +4,7 @@ gsap.registerPlugin(ScrollTrigger);
 
 var html = document.querySelector("html");
 var body = document.querySelector("body");
+var headerBlock = document.querySelector("header");
 
 // // pc/모바일 분기 처리
 var Init = {
@@ -38,13 +39,12 @@ var Init = {
   },
 };
 
-Init.defaults();
 
-
-
+// 헤더
 var Header = {
   menuButtonEl: document.querySelector(".menu-button"),
   gnbEl: document.querySelector("header .gnb"),
+  gnbAnchrEl: document.querySelector("header .gnb a"),
   init: function () {
     this.menu();
   },
@@ -58,23 +58,114 @@ var Header = {
       } else {
         playScroll();
       }
-
+      // Common.focusLoop(gnbEl);
+      Common.focusLoop(headerBlock);
     })
   },
 
   scroll: function () {
-    
+
   },
 }
 
-Header.init();
 
+//about 
+var About = {
+  activeTextEls: document.querySelectorAll(".active-text"),
+  init: function () {
+    this.observe();
+  },
+  observe: function () {
+    const io = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        console.log(entry) // entry is 'IntersectionObserverEntry'
+        // 가시성의 변화가 있으면 관찰 대상 전체에 대한 콜백이 실행되므로,
+        // 관찰 대상의 교차 상태가 false일(보이지 않는) 경우 실행하지 않음.
+        if (!entry.isIntersecting) {
+          entry.target.classList.remove("active");
+          return
+        }
+        // 관찰 대상의 교차 상태가 true일(보이는) 경우 실행.
+        entry.target.classList.add("active");
+      })
+    },
+      { threshold: 0.8 }
+    )
 
-//html 스크롤
-function playScroll() {
-  html.classList.remove("fixed");
+    About.activeTextEls.forEach(el => io.observe(el))
+  }
 }
-//html 고정
+
+
+
+var Common = {
+  init: function () {
+    this.focusLoop();
+    this.firstElementFocus();
+  },
+  focusLoop: function (element) {
+    var focusableElementsString = 'a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, embed, [tabindex="0"], [contenteditable]';
+    var focusableElements = element.querySelectorAll(focusableElementsString);
+    focusableElements = Array.prototype.slice.call(focusableElements);
+    var firstTabStop = focusableElements[0];
+    var lastTabStop = focusableElements[focusableElements.length - 1];
+
+    firstTabStop.focus();
+    element.addEventListener("keydown", trapTabKey);
+
+    function trapTabKey(e) {
+      // Check for TAB key press
+      if (e.keyCode === 9) {
+        // SHIFT + TAB
+        if (e.shiftKey) {
+          if (document.activeElement === firstTabStop) {
+            e.preventDefault();
+            lastTabStop.focus();
+          }
+          // TAB
+        } else {
+          if (document.activeElement === lastTabStop) {
+            e.preventDefault();
+            firstTabStop.focus();
+          }
+        }
+      }
+      // ESCAPE
+      if (e.keyCode === 27) {
+        //closeModal();
+      }
+    }
+  },
+  firstElementFocus: function (element) {
+    var focusableElementsString = 'a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, embed, [tabindex="0"], [contenteditable]';
+    var focusableElements = element.querySelectorAll(focusableElementsString);
+    focusableElements = Array.prototype.slice.call(focusableElements);
+    var firstTabStop = focusableElements[0];
+    var lastTabStop = focusableElements[focusableElements.length - 1];
+
+    firstTabStop.focus();
+  },
+}
+
+
+console.log(typeof Common);
+
+
+// --------------------------------------------------------------
+// --------------------------------------------------------------
+
+Init.defaults();
+Header.init();
+About.init();
+
+// --------------------------------------------------------------
+// --------------------------------------------------------------
+
+//body 스크롤
+function playScroll() {
+  html.classList.remove("stop-scroll");
+}
+//body 고정
 function stopScroll() {
-  html.classList.add("fixed");
+  html.classList.add("stop-scroll");
 }
